cc250e76b2c6a9b8a0b830ae1ad159c9f61fc586
HMWB Academy <hmwbacademy@gmail.com>
2025-04-03 16:45:27 +0530

    raising error and fixing it correctly

--- bug_fix.py
+++ bug_fix.py
@@ -1,8 +1,9 @@
 def bug_fix(error):
+    raise_error(error)
     return "Bug is fixed"
 
 def raise_error(error):
     print(f"Error Occured: {error}")
 
 if __name__ == "__main__":
-    bug_fix()
+    bug_fix("Issue is NameError")
